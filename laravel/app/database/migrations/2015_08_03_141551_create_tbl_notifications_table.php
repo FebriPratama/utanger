f47b0fe2a5cb589a8fd55d900d62c3b9454fc790
unknown <openshiftvendpad@gmail.com>
2016-05-12 19:24:19 +0700

    init ionic

---
+++ laravel/app/database/migrations/2015_08_03_141551_create_tbl_notifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTblNotificationsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tbl_notifications',function(Blueprint $table){
+
+			$table->increments('notif_id');
+			$table->integer('notif_user_id');
+			$table->integer('notif_type_id');
+			$table->string('notif_status');
+			$table->datetime('notif_last_sync');
+			$table->string('notif_sync_status');
+			$table->timestamps();
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		
+		Schema::drop('tbl_notifications');
+
+	}
+
+}
