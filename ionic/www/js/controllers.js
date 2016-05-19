4880507e1ac33f494204c955b2c4c277ff17c5a6
pramayu <gedepramayu2@gmail.com>
2016-05-19 20:27:55 +0700

    pop up message

--- ionic/www/js/controllers.js
+++ ionic/www/js/controllers.js
@@ -6,35 +6,55 @@ utanger
 	};
 
 	//modal add utang
+	// modal 1
 	$ionicModal.fromTemplateUrl('templates/add_utang.html', {
-		scope: $scope,
-		animation: 'slide-in-up'
-	}).then(function(modal) {
-		$scope.modal = modal;
-	});
-	
-	$scope.openModal = function() {
-		$scope.modal.show();
-	};
-	
-	$scope.closeModal = function() {
-		$scope.modal.hide();
-	};
-	
-   //Cleanup the modal when we're done with it!
-   $scope.$on('$destroy', function() {
-   	$scope.modal.remove();
-   });
+      id: '1', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      backdropClickToClose: false,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.oModal1 = modal;
+    });
+    // modal 2
+     // Modal 2
+    $ionicModal.fromTemplateUrl('templates/new_msg.html', {
+      id: '3', // We need to use and ID to identify the modal that is firing the event!
+      scope: $scope,
+      // backdropClickToClose: false,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.oModal2 = modal;
+    });
+    $scope.openModal = function(index) {
+      if (index == 1) $scope.oModal1.show();
+      else $scope.oModal2.show();
+    };
 
-   // Execute action on hide modal
-   $scope.$on('modal.hidden', function() {
-      // Execute action
-  });
+    $scope.closeModal = function(index) {
+      if (index == 1) $scope.oModal1.hide();
+      else $scope.oModal2.hide();
+    };
 
-   // Execute action on remove modal
-   $scope.$on('modal.removed', function() {
-      // Execute action
-  });
+    /* Listen for broadcasted messages */
+
+    $scope.$on('modal.shown', function(event, modal) {
+      console.log('Modal ' + modal.id + ' is shown!');
+    });
+
+    $scope.$on('modal.hidden', function(event, modal) {
+      console.log('Modal ' + modal.id + ' is hidden!');
+    });
+
+    // Cleanup the modals when we're done with them (i.e: state change)
+    // Angular will broadcast a $destroy event just before tearing down a scope 
+    // and removing the scope from its parent.
+    $scope.$on('$destroy', function() {
+      console.log('Destroying modals...');
+      $scope.oModal1.remove();
+      $scope.oModal2.remove();
+    });
+
+
     // map 
    function initialize() {
    	var myLatlng = new google.maps.LatLng(-7.79009,110.3618989);
